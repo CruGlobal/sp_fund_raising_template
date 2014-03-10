fafd85887ebce2f572cd226038b926080ac30fff
Josh Starcher <josh.starcher@gmail.com>
2014-03-10 10:15:47 -0400

    Uncomment designation number and motivation code as options

--- options.php
+++ options.php
@@ -31,7 +31,7 @@ function optionsframework_options() {
 		'std' => '0.00',
 		'class' => 'mini',
 		'type' => 'text');
-		
+
   $options[] = array(
 		'name' => __('Amount Raised', 'options_framework_theme'),
 		'desc' => __('You&rsquo;ll have to manually change this on a regular basis to account for donations not received through this site', 'options_framework_theme'),
@@ -39,7 +39,7 @@ function optionsframework_options() {
 		'std' => '0.00',
 		'class' => 'mini',
 		'type' => 'text');
-		
+
   $options[] = array(
 		'name' => __('Donation End Date', 'options_framework_theme'),
 		'desc' => __('This is typically the day given to you by Cru staff as your donation deadline', 'options_framework_theme'),
@@ -67,21 +67,21 @@ function optionsframework_options() {
 		'desc' => __('This image will be shown on the main page of your SP website. Feel free to use any size image bigger 608px X 52px.', 'options_framework_theme'),
 		'id' => 'spkick_project_image',
 		'type' => 'upload');
-		
+
   $options[] = array(
 		'name' => __('Project Video', 'options_framework_theme'),
 		'desc' => __('A video is cooler than a project image. If you shoot a video of yourself describing your project, upload it to youtube and paste the url here.', 'options_framework_theme'),
 		'id' => 'spkick_video_url',
 		'std' => '',
 		'type' => 'text');
-		
+
 	$options[] = array(
 		'name' => __('Full Project Description', 'options_framework_theme'),
 		'desc' => __('Enter a full project description. This is a great way to give details about your project and invite your supporters in with you.', 'options_framework_theme'),
 		'id' => 'spkick_fulldescription',
 		'std' => "Example: We'll be going to an exciting part of the world to share our faith with students and various faculty. I'm excited at the possibility to share in a language I don't speak",
-		'type' => 'textarea');	
-		
+		'type' => 'textarea');
+
 
 	$options[] = array(
 		'name' => __('Your Name', 'options_framework_theme'),
@@ -89,7 +89,7 @@ function optionsframework_options() {
 		'id' => 'spkick_person_name',
 		'std' => 'Merdigal Magilicutootles',
 		'type' => 'text');
-		
+
   /*
 $options[] = array(
 		'name' => __('Your Occupation', 'options_framework_theme'),
@@ -105,29 +105,29 @@ $options[] = array(
 		'id' => 'spkick_person_bio',
 		'std' => 'Keep us engaged!',
 		'type' => 'textarea');
-  
+
   $options[] = array(
 		'name' => __('Your Picture', 'options_framework_theme'),
 		'desc' => __('This will get turned into a square.', 'options_framework_theme'),
 		'id' => 'spkick_person_image',
 		'type' => 'upload');
-	
-	
-//	$options[] = array(
-//		'name' => __('Designation Number', 'options_framework_theme'),
-//		'desc' => __('', 'options_framework_theme'),
-//		'id' => 'spkick_designation',
-//		'std' => '',
-//		'class' => 'mini',
-//		'type' => 'text');
-		
-//  $options[] = array(
-//		'name' => __('Campaign Code', 'options_framework_theme'),
-//		'desc' => __('', 'options_framework_theme'),
-//		'id' => 'spkick_motivation',
-//		'std' => '',
-//		'class' => 'mini',
-//		'type' => 'text');
-	
+
+
+  $options[] = array(
+    'name' => __('Designation Number', 'options_framework_theme'),
+    'desc' => __('', 'options_framework_theme'),
+    'id' => 'spkick_designation',
+    'std' => '',
+    'class' => 'mini',
+    'type' => 'text');
+
+  $options[] = array(
+    'name' => __('Campaign Code', 'options_framework_theme'),
+    'desc' => __('', 'options_framework_theme'),
+    'id' => 'spkick_motivation',
+    'std' => '',
+    'class' => 'mini',
+    'type' => 'text');
+
 	return $options;
 }
